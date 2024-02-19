2c8911d87c320b3ec59edf44f09c3c134d271c9a
jaburger <jaburger@udel.edu>
2024-02-18 20:40:03 -0500

    completed removeDollars

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,6 +5,9 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
+    if (numbers.length === 0) {
+        return [];
+    }
     const newNums: number[] = [numbers[0], numbers[numbers.length - 1]];
     return newNums;
 }
