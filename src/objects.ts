25be97699f62a8e76fa92a2f117b44653048018b
jaburger <jaburger@udel.edu>
2024-02-23 12:47:28 -0500

    completed toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -87,7 +87,15 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let output: string = "# " + question.name + "\n" + question.body;
+    if (question.type === "multiple_choice_question") {
+        question.options.map((option: string): boolean => {
+            output += "\n- " + option;
+            return true;
+        });
+    }
+
+    return output;
 }
 
 /**
