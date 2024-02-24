2ef6b50aafc10e7ebd60b9da144daa617fdd1c7a
jaburger <jaburger@udel.edu>
2024-02-24 18:28:48 -0500

    completed sumPoints

--- src/nested.ts
+++ src/nested.ts
@@ -94,7 +94,10 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    return questions.reduce(
+        (sum: number, question: Question): number => sum + question.points,
+        0
+    );
 }
 
 /***
