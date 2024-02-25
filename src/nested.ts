624aa2d435d90389315c7aacc25f6c185136f94b
jaburger <jaburger@udel.edu>
2024-02-24 19:11:40 -0500

    completed sameType

--- src/nested.ts
+++ src/nested.ts
@@ -186,7 +186,15 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    if (questions.length === 0) {
+        return true;
+    }
+
+    return questions.reduce(
+        (same: boolean, question: Question): boolean =>
+            same && questions[0].type === question.type,
+        true
+    );
 }
 
 /***
