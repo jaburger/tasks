b4636050d129120474549f398c80b4e2d6e2ec0c
jaburger <jaburger@udel.edu>
2024-02-28 12:37:42 -0500

    completed changeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,23 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [currType, swapType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    const typeSwitchMap: Record<QuestionType, QuestionType> = {
+        multiple_choice_question: "short_answer_question",
+        short_answer_question: "multiple_choice_question"
+    };
+    function toggleType(): void {
+        swapType(typeSwitchMap[currType]);
+    }
+    return (
+        <div>
+            <Button onClick={toggleType}>Change Type</Button>
+            {currType === "multiple_choice_question" && (
+                <div>Multiple Choice</div>
+            )}
+            {currType === "short_answer_question" && <div>Short Answer</div>}
+        </div>
+    );
 }
